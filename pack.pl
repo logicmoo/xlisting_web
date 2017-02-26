0785ce6e8b2346faa9e3a7057fe30193d0ba22b9
root <logicmoo@gmail.com>
2017-02-25 22:03:33 -0800

    v0.0.8

---
+++ pack.pl
@@ -0,0 +1,10 @@
+name(xlisting_web).
+version('0.0.8').
+title('New simple loop checking').
+keywords([source,goal_expansion]).
+author( 'Douglas Miles', 'http://www.linkedin.com/in/logicmoo' ).
+packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
+maintainer( 'TeamSPoon', 'https://github.com/TeamSPoon/' ).
+home( 'https://github.com/TeamSPoon/loop_check' ).
+download( 'https://github.com/TeamSPoon/loop_check/release/*.zip' ).
+requires(xlisting).
